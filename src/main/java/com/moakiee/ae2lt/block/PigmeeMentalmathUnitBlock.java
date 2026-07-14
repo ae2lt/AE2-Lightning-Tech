a13a3987c4dbd5b7f05f4d2d6636b57348bda3d1
MOAKIEE <1324227985@qq.com>
2026-07-14 22:18:01 +0800

    添加toollip

--- src/main/java/com/moakiee/ae2lt/block/PigmeeMentalmathUnitBlock.java
+++ src/main/java/com/moakiee/ae2lt/block/PigmeeMentalmathUnitBlock.java
@@ -1,12 +1,19 @@
 package com.moakiee.ae2lt.block;
 
+import java.util.List;
+
 import appeng.api.orientation.IOrientationStrategy;
 import appeng.api.orientation.OrientationStrategies;
 import appeng.block.AEBaseEntityBlock;
 
 import com.moakiee.ae2lt.blockentity.PigmeeMentalmathUnitBlockEntity;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.core.Direction;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item.TooltipContext;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 
@@ -21,5 +28,18 @@ public class PigmeeMentalmathUnitBlock extends AEBaseEntityBlock<PigmeeMentalmat
     @Override
     public IOrientationStrategy getOrientationStrategy() {
         return OrientationStrategies.horizontalFacing();
+    }
+
+    @Override
+    public void appendHoverText(
+            ItemStack stack,
+            TooltipContext context,
+            List<Component> tooltipComponents,
+            TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        tooltipComponents.add(Component.translatable("tooltip.ae2lt.pigmee_mentalmath_unit.1")
+                .withStyle(ChatFormatting.GRAY));
+        tooltipComponents.add(Component.translatable("tooltip.ae2lt.pigmee_mentalmath_unit.2")
+                .withStyle(ChatFormatting.LIGHT_PURPLE));
     }
 }
